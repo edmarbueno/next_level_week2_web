a34ffaa3c02574784708e26530313630874c1dc5
Edmar Bueno <bueno.edmar@gmail.com>
2020-08-20 22:35:53 -0300

    Adiconando projeto web

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import api from "../../services/api";
+
+import "./styles.css";
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          // src="https://avatars3.githubusercontent.com/u/14100622?s=460&u=efb6c9c4db19ebbbc7a6b2aa43bdd3ee0da4c742&v=4"
+          src={teacher.avatar}
+          alt={teacher.name}
+        />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+      <p>{teacher.bio}</p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
